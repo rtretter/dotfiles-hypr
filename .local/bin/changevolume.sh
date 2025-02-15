096def0443908398487fc19503938e663494174d
rtretter <25963632+rtretter@users.noreply.github.com>
2025-02-15 22:42:46 +0100

    added dunst and volume controls

---
+++ .local/bin/changevolume.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function send_notification() {
+	volume=$(pamixer --get-volume)
+	dunstify -i ~/.config/dunst/icons/player-volume.svg -a "changevolume" -u normal -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
+}
+
+case $1 in
+up)
+	# Set the volume on (if it was muted)
+	pamixer -u
+	volume=$(pamixer --get-volume)
+	if (($volume >= 145)); then
+		pamixer --set-volume 150 --allow-boost
+	else
+		pamixer -i 5 --allow-boost
+	fi
+	send_notification $1
+	;;
+down)
+	pamixer -u
+	pamixer -d 5 --allow-boost
+	send_notification $1
+	;;
+mute)
+	pamixer -t
+	if $(pamixer --get-mute); then
+		dunstify -i ~/.config/dunst/icons/player-volume-muted.svg -a "changevolume" -t 2000 -r 9993 -u low "Sound muted!"
+	else
+		send_notification up
+	fi
+	;;
+esac
